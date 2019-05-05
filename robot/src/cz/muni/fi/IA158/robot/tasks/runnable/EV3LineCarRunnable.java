04ddbcc1dd79b32880215b4612047bc1186bb529
anohejlova <nohejlova.a@seznam.cz>
2019-05-05 16:11:16 +0200

    Thread suggestion

--- robot/src/cz/muni/fi/IA158/robot/tasks/runnable/EV3LineCarRunnable.java
+++ robot/src/cz/muni/fi/IA158/robot/tasks/runnable/EV3LineCarRunnable.java
@@ -9,13 +9,47 @@ public class EV3LineCarRunnable {
     	BlockingQueue<Job> queueDist = new ArrayBlockingQueue<>(10);
     	BlockingQueue<Job> queueSteer = new ArrayBlockingQueue<>(10);
     	
-    	SchedulerRunnable scheduler = new SchedulerRunnable(queueDist, queueSteer);
-    	new Thread(scheduler).start();
+    	
     	
     	SteeringRunnable steering = new SteeringRunnable(queueSteer);
-    	new Thread(steering).start();
     	DistanceCheckRunnable distance = new DistanceCheckRunnable(queueDist);
+    	Thread steerThread = new Thread(steering);
+    	Thread distThread = new Thread(distance);
+    	steerThread.start();
+    	distThread.start();
     	new Thread(distance).start();
+    	
+    	Job distJob = null;
+    	Job steerJob = null;
+    	
+    	while (true) {  //should be replaced by sensible condition
+    		
+	    	if (distJob == null) {
+	    		
+				distJob = queueDist.poll();
+			}
+	    	if (steerJob == null) {
+	    		
+	    		steerJob = queueSteer.poll();
+			}
+			if ((distJob != null) && (steerJob != null)) {
+				
+				if (distJob.getDeadline() < steerJob.getDeadline()) {
+					distThread.run();
+					distJob = null;
+				} else {
+					steerThread.run();
+					steerJob = null;
+				}
+			} else if (steerJob != null) {
+				steerThread.run();
+				distJob = null;
+			} else if (distJob != null) {
+				distThread.run();
+				steerJob = null;
+			}
+    	}
+		
     	
     	//long millis = System.currentTimeMillis(); // number of milliseconds from start of the epoch
     	
