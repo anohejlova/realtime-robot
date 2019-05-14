cbd61d9ce07296f68432fd9e94f44b9287fe2c78
Prometheus159 <50074419+Prometheus159@users.noreply.github.com>
2019-05-14 13:56:39 +0200

    DIstance done

--- robot/src/cz/muni/fi/IA158/robot/tasks/runnable/DistanceCheckRunnable.java
+++ robot/src/cz/muni/fi/IA158/robot/tasks/runnable/DistanceCheckRunnable.java
@@ -11,7 +11,7 @@ public class DistanceCheckRunnable implements Runnable{
 	boolean suspended = false;
 	private BlockingQueue<Job> queueDist;
 	long releaseDeadlineDiff = 200;
-	private int lastDistance = 0;
+	private int lastDistance = 100;
 	private int newDistance;
 	private EV3IRSensor ir; 
 	private EV3LargeRegulatedMotor powerMotor;
@@ -47,9 +47,9 @@ public class DistanceCheckRunnable implements Runnable{
 			    	}		    	
 			    }
 			    
-			  /*if (checkDistance()) {
+			  if (checkDistance()) {
 				  adaptSpeed();
-			  }*/
+			  }
 			}  
 	    } catch (InterruptedException e) {
 	       System.out.println("distThread interrupted.");
@@ -60,7 +60,12 @@ public class DistanceCheckRunnable implements Runnable{
 	private boolean checkDistance() {
 		
 		lastDistance = newDistance;
-		newDistance = ir.getDistanceMode().sampleSize();
+		
+		float [] sample = new float[ir.sampleSize()];
+	    ir.fetchSample(sample, 0);
+	    newDistance = (int)sample[0];	
+		
+		
 		int diff = java.lang.Math.abs(newDistance - lastDistance);
 		
 		return ((diff > 10) || (newDistance < 50));
@@ -70,6 +75,7 @@ public class DistanceCheckRunnable implements Runnable{
 	private void adaptSpeed() {
 		
 		if (newDistance <= 20) {
+			System.out.println("STOP" + newDistance + " " + lastDistance);
 			powerMotor.stop();
 			return;
 		} 
@@ -79,16 +85,23 @@ public class DistanceCheckRunnable implements Runnable{
 					
 		if (diff <= -60) {
 			powerMotor.setSpeed((int) Math.round(0.2 *curSpeed));
+			System.out.println(" sub -60");
 		} else if (diff <= -40) {
 			powerMotor.setSpeed((int) Math.round(0.4 *curSpeed));
+			System.out.println("sub -40");
 		} else if (diff <= -20) {
 			powerMotor.setSpeed((int) Math.round(0.6 *curSpeed));
+			System.out.println("sub -20");
 		} else if (diff < 10) {
 			powerMotor.setSpeed((int) Math.round(0.8 *curSpeed));
-		} else if ((diff > 60) &&  (curSpeed == 0)) {
-			powerMotor.setSpeed((int) Math.round(0.2 * powerMotor.getMaxSpeed()));
+			System.out.println("sub 10");
+		} else if ((diff > 60) && (lastDistance <= 20)) {
+			powerMotor.setSpeed(180);
+			powerMotor.backward();
+			System.out.println("restart");
 		} else if (diff > 10) {
 			powerMotor.setSpeed((int) Math.round(1.2 *curSpeed));
+			System.out.println("over 10");
 		}
 	}
 	
