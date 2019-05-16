3849e972094be204fc80a4da1edf52c7322c7be4
Prometheus159 <50074419+Prometheus159@users.noreply.github.com>
2019-05-17 01:44:31 +0200

    Comments added to job class

--- robot/src/cz/muni/fi/IA158/robot/tasks/runnable/Job.java
+++ robot/src/cz/muni/fi/IA158/robot/tasks/runnable/Job.java
@@ -1,9 +1,14 @@
 package cz.muni.fi.IA158.robot.tasks.runnable;
 
+/**
+ * Class representing jobs for each task *
+ */
 public class Job {
+	//attributes of each job
     private long releaseTime;
     private long deadline;
     
+    //constructor for new job
     public Job(long releaseTime, long deadline){
         this.releaseTime=releaseTime;
         this.deadline=deadline;
